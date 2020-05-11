0526fe53c177d8647258cafde758529abb99a92b
Georgios Tagkoulis <giorgos.tagkoulis@gmail.com>
2020-05-11 18:48:12 +0200

    finalise server

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-// const fetch = require('node-fetch');
+const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { states } = require('./statesOfUS');
 
 const app = express();
 
@@ -9,11 +10,60 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
-// const notFound = 404;
+const getTodayDate = () => {
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  let mm = today.getMonth() + 1;
+  let dd = today.getDate();
+  mm = mm < 10 ? `0${mm}` : mm;
+  dd = dd < 10 ? `0${dd}` : dd;
+  return yyyy + mm + dd;
+};
 
-app.get('/', (_req, res) => {
-  res.sendCode(200);
-});
+let cachedData = {
+  date: null,
+  resources: [],
+};
+
+const fetchData = (res) => {
+  const requests = states.map((state) =>
+    fetch(
+      `https://covidtracking.com/api/v1/states/${state.short}/daily.json`,
+    ),
+  );
+
+  Promise.all(requests)
+    .then((responses) => Promise.all(responses.map((r) => r.json())))
+    .then((responses) =>
+      responses.map((r, index) => {
+        return {
+          stateName: states[index].name,
+          hospitilized: r[0].hospitalizedCurrently,
+          deaths: r
+            .slice(0, 3)
+            .reduce((acc, cur) => acc + cur.death, 0),
+        };
+      }),
+    )
+    .then((response) => {
+      cachedData = {
+        date: getTodayDate(),
+        resources: response,
+      };
+      return cachedData;
+    })
+    .then((response) => res.json(response))
+    .catch((err) => {
+      console.error(new Error(err.message));
+      res.sendStatus(500);
+    });
+};
+
+app.get('/data', (_req, res) =>
+  getTodayDate() === cachedData.date
+    ? res.json(cachedData)
+    : fetchData(res),
+);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
